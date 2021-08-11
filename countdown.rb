772dc2a06101b8a612c24d7d1be938780d2295f9
Trace <tracedelange@me.com>
2021-08-11 14:12:15 -0500

    commit tree 3c6a53d8c1115ce93fa28b08d911a5f2ed0082bc

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(starting_i)
+    i = 0
+    while i < starting_i
+        puts "#{starting_i - i} SECOND(S)!"
+        i += 1
+    end
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(starting_i)
+    i = 0
+    while i < starting_i
+        puts "#{starting_i - i} SECOND(S)!"
+        i += 1
+        sleep(1)
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+
+# countdown(10)
